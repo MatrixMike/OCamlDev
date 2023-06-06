babc7a17e6addc0679ddbcd810518496dde32fb3
Mike Hewitt <matroxmike@icloud.com>
2023-06-06 21:30:55 +1000

    simple debug

--- gcd.ml
+++ gcd.ml
@@ -1,7 +1,14 @@
 (* File gcd.ml *)
+(*
+06.06.2023 21:10:36
+*)
+
 let rec gcd a b =
   if b = 0 then a
-  else gcd b (a mod b);;
+  else begin 
+  Printf.printf "%d %d\n" a b;
+  gcd b (a mod b)
+  end;;
 
 let main () =
   let a = int_of_string Sys.argv.(1) in
